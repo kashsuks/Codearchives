71fd82c87800ef6f70bd7fef452383bc17c31c51
Kashyap <ksukshavasi@gmail.com>
2025-03-05 01:56:53 -0500

    oath setup stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const handleLogin = () => {
+        const clientId = process.env.REACT_APP_GITHUB_CLIENT_ID; // Use environment variable
+        const redirectUri = encodeURIComponent(process.env.REACT_APP_GITHUB_REDIRECT_URI); // Use environment variable
+        const scope = 'user:email';
+        const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
+        window.location.href = authUrl;
+    };
+
     return (
         <nav className="navbar">
             <div className="container">
@@ -11,7 +19,7 @@ function Navbar() {
                     <li><Link to="/problems">Problems</Link></li>
                     <li><Link to="/contests">Contests</Link></li>
                     <li><Link to="/rankings">Rankings</Link></li>
-                    <li><Link to="/login">Login</Link></li>
+                    <li><button onClick={handleLogin}>Login</button></li>
                 </ul>
             </div>
         </nav>
